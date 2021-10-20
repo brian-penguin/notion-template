a9c3f847cb5116c7682a41a2e71c3939220f6d94
Brian Tenggren <tenggren@thoughtbot.com>
2021-10-19 23:09:43 -0400

    Update the readme and cleanup logging

--- README.md
+++ README.md
@@ -10,13 +10,14 @@ I then fill it with a quick "how I'm feeling" section, a personal todo list, and
 ### What this is
 This is a script that's going to do some of that daily templating for me. It should be able to:
 - [x] Create a new page with the date as the title
-- [] Populate the weather (this would be cool to do dynamically based on my location but first pass is my default work location)
-- [] Start the two todo lists I use most
+- [x] Populate the weather (this would be cool to do dynamically based on my location but first pass is my default work location)
+- [x] Start the two todo lists I use most
 - [] A dumb quote or tweet or something stupid in my timeline
+- [] ???
+- [] Profit
 
 ### Dependencies
 - This script uses babashka as a native scripting tool. It uses the shebang syntax to point to a usr/local/bb symlink. You can find out how to install that here: https://github.com/babashka/babashka (I brew installed it)
--
 
 ### Setting up
 - You will need to create a new integration
@@ -24,6 +25,7 @@ This is a script that's going to do some of that daily templating for me. It sho
 - You will need to figure out your database id
 - You will add both of those to a config.edn file at the root of your project (see config-example.edn)
 - You will need to add that integration to your database / page
+- You can figure out your weather service url here https://www.weather.gov/documentation/services-web-api
 
 ### Creating a Page in Notion
 Here's an example curl request
---
+++ create_daily_notion.clj
@@ -0,0 +1,87 @@
+#!/usr/bin/env bb
+
+(require '[babashka.curl :as curl])
+(require '[clojure.edn :as edn])
+(require '[cheshire.core :as json])
+(require 'clojure.pprint)
+
+(import 'java.time.format.DateTimeFormatter
+        'java.time.LocalDateTime)
+
+(def date (LocalDateTime/now))
+(def formatter (DateTimeFormatter/ofPattern "EEEE MMMM dd yyyy"))
+(def daily-notion-page-title
+  (.format date formatter)) ;;=> "Tuesday October 19 2021"
+
+(def config (edn/read-string (slurp "config.edn")))
+(def journal-database-id (:JOURNAL_ID config))
+(def notion-api-key (:NOTION_API_KEY config))
+;; you can figure out your url https://www.weather.gov/documentation/services-web-api
+(def forecast-url (:FORECAST_URL config))
+
+(def forecast
+  (-> (curl/get forecast-url {:throw false})
+      :body
+      (json/parse-string true)
+      :properties
+      :periods))
+
+(defn forecast-period->content-block [forecast-period-data]
+  (let [period-name (:name forecast-period-data)
+        forecast (:detailedForecast forecast-period-data)
+        forecast-string (str period-name ": " forecast)]
+   {:object :block
+    :type :bulleted_list_item
+    :bulleted_list_item {:text [{:type :text :text {:content forecast-string}}]}}))
+
+(def weather-content
+  (map forecast-period->content-block (take 3 forecast)))
+
+(def page-content
+  (concat [{:object :block
+            :type :heading_2
+            :heading_2 {:text [{:type :text :text {:content "Today"}}]}}]
+          weather-content
+          [{:object :block
+            :type :heading_2
+            :heading_2 {:text [{:type :text :text {:content "I'm feeling..."}}]}}
+           {:object :block
+            :type :paragraph
+            :paragraph {:text [{:type :text :text {:content " "}}]}}
+           {:object :block
+            :type :heading_3
+            :heading_3 {:text [{:type :text :text {:content "Work Stuff"}}]}}
+           {:object :block
+            :type :to_do
+            :to_do {:checked false
+                    :text [{:type :text :text {:content "Generate Daily Notes"}}]}}
+           {:object :block
+            :type :heading_3
+            :heading_3 {:text [{:type :text :text {:content "Personal"}}]}}
+           {:object :block
+            :type :to_do
+            :to_do {:checked false
+                    :text [{:type :text :text {:content "..."}}]}}]))
+
+(def request-url "https://api.notion.com/v1/pages")
+(def request-headers
+  {"Authorization" notion-api-key
+   "Content-Type" "application/json"
+   "Notion-Version" "2021-08-16"})
+(def request-body
+  {:parent {:database_id journal-database-id}
+   :properties {"Name" {:title [{:text {:content daily-notion-page-title}}]}
+                "Stuff" {:multi_select [{:name "Daily"} {:name "Work"}]}}
+   :children page-content})
+
+(println "Beep Boop")
+(println "Creating a new Notion Journal entry:" daily-notion-page-title)
+(def response (curl/post request-url
+                         {:body (json/generate-string request-body)
+                          :headers request-headers
+                          :throw false}))
+(println (str "Notion says: " (:status response)))
+
+
+
+
